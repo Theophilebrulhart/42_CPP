160ca9883b2126f16839b35ca03a2b7fef0e4299
Theo <tbrulhar@student.42lausanne.ch>
2023-01-20 14:09:43 +0100

    Module 05

--- module 05/ex03/main.cpp
+++ module 05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tbrulhar <tbrulhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 15:31:20 by tbrulhar          #+#    #+#             */
-/*   Updated: 2023/01/20 12:23:50 by tbrulhar         ###   ########.fr       */
+/*   Updated: 2023/01/20 14:06:44 by tbrulhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,32 @@
 #include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "Intern.hpp"
 
 int main()
 {
 	// AForm FormA("test", 2); -> pas possible car Aform est abstrait;
 	//ShrubberyCreationForm	*FormSchrub = new ShrubberyCreationForm("Jardin");
 	//RobotomyRequestForm		*FormSchrub = new RobotomyRequestForm("Ta mere");
-	PresidentialPardonForm		*FormSchrub = new PresidentialPardonForm("Pierre");
-	Bureaucrat	Claude("Claude", 3);
-	Bureaucrat	Hubert("Hubert", 150);
+	// PresidentialPardonForm		*FormSchrub = new PresidentialPardonForm("Pierre");
+	 Bureaucrat	Claude("Claude", 3);
+	//Bureaucrat	Hubert("Hubert", 150);
 
-	std::cout << FormSchrub->getExecGrade() << std::endl;
-	FormSchrub->execute(Claude);
-	Hubert.executeForm(*FormSchrub);
-	Hubert.signAForm(*FormSchrub);
-	Claude.signAForm(*FormSchrub);
-	FormSchrub->execute(Claude);
+	// std::cout << FormSchrub->getExecGrade() << std::endl;
+	// FormSchrub->execute(Claude);
+	// Hubert.executeForm(*FormSchrub);
+	// Hubert.signAForm(*FormSchrub);
+	// Claude.signAForm(*FormSchrub);
+	// FormSchrub->execute(Claude);
 
-	Hubert.executeForm(*FormSchrub);
+	// Hubert.executeForm(*FormSchrub);
 
-	delete(FormSchrub);
+	// delete(FormSchrub);
+	Intern someRandomIntern;
+	AForm* rrf;
+	rrf = someRandomIntern.makeForm("Robotomy request", "Bender");
+	Claude.signAForm(*rrf);
+	
 
 	
 	return (0);
